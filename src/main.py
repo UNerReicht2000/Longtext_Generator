37d2837098ff87f7fa051dd289eec5d820fff1dc
Dennis Unger <dennis.unger@bbsautomation.com>
2025-08-09 19:30:09 +0200

    Finish main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from dev_func import debug_print
-
 from core.kuka import Longtext
-
 from user_ui.main_ui import MainWindowLtG
-
-def __init__() -> None:
-    pass
-
 
 if __name__ == '__main__':
     ui = MainWindowLtG()
