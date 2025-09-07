3484c9b160b76eabf714685e1f1eaa676e0d860e
UNerReicht2000 <unger.dennis20@gmail.com>
2025-09-07 22:42:10 +0700

    Add license information and copyright notice to source files

--- src/core/longtext_func.py
+++ src/core/longtext_func.py
@@ -1,4 +1,22 @@
 ﻿# -*- coding: utf-8 -*-
+#
+# Longtext Generator for KRL
+# Copyright (C) 2025  Dennis Unger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# --------------------------------------------------------------------
 from os import path
 from os import access
 from os import R_OK
